257f791a2b5ae736ede944f4e7d5a8223469e713
Wesley Maffly-Kipp <maffkipp@yahoo.com>
2018-02-18 20:40:33 -0600

    added post route

--- app/forms.py
+++ app/forms.py
@@ -5,11 +5,11 @@ from wtforms.validators import DataRequired, Length
 
 class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
-    content = StringField('Content')
+    content = TextAreaField('Content')
     name = StringField('Name', validators=[Length(min=0, max=50)])
     submit = SubmitField('Post')
 
 
 class CommentForm(FlaskForm):
-    content = StringField('Content')
+    content = TextAreaField('Content')
     name = StringField('Name', validators=[Length(min=0, max=50)])
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect, url_for, request
 from app.models import Comment, Post
 from app.forms import PostForm, CommentForm
 
@@ -7,5 +7,17 @@ from app.forms import PostForm, CommentForm
 @app.route('/')
 @app.route('/index')
 def index():
+    return redirect(url_for('posts'))
+
+
+@app.route('/posts', methods=['GET', 'POST'])
+def posts():
     form = PostForm()
-    return render_template('index.html', form=form)
+    if form.validate_on_submit():
+        new_post = Post(title=form.title)
+        new_post.content = form.content or ''
+        new_post.name = form.name or 'anonymous_user'
+        new_post.save()
+        print('saved post to db')
+        return redirect(url_for('posts'))
+    return render_template('index.html', form=form, posts=Post.objects)
